c224bfd32d3a86d01276db9bdbaff609ddc19606
shadman-a <shadman@outlook.com>
2020-10-08 17:55:40 -0400

    i pushed

--- src/Home.js
+++ src/Home.js
@@ -39,7 +39,7 @@ export default class Home extends Component {
             buisnessArray:result
         }),
         this.setState({
-            search: ""
+            search: " "
         })
     )}
   
