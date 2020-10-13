527eb96a9a653acab18d943bd931661ed3f44050
shadman-a <shadman@outlook.com>
2020-10-12 21:52:47 -0400

    map added?

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import logo from './logo.svg';
 import './App.css';
 import { BrowserRouter as Router, Route } from "react-router-dom";
 
+
 import Home from './Home'
 import Map from './Map'
 import NavBar from './components/NavBar'
@@ -11,12 +12,11 @@ import NavBar from './components/NavBar'
 function App() {
   return (
     <Router>
-    <div>
       <NavBar />
       <Route exact path="/" component={Home} />
       <Route exact path="/map" component={Map} />
-    </div>
   </Router>
+
     
   );
 }
