db1ef4bb560837ade7a0311b5a0ee5626435d416
shadman-a <shadman@outlook.com>
2020-10-15 01:22:50 -0400

    just a commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,7 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 import { BrowserRouter as Router, Route } from "react-router-dom";
 
-import {
-  Card,
-  CardActionArea,
-  CardActions,
-  CardContent,
-  CardMedia,
-  Button,
-  Grid,
-  AppBar,
-  Toolbar,
-  Typography,
-  IconButton,
-} from "@material-ui/core";
-import { FaBars } from 'react-icons/fa'
-import { FaLocationArrow } from 'react-icons/fa'
 
 import Home from './Home'
 import Map from './Map'
